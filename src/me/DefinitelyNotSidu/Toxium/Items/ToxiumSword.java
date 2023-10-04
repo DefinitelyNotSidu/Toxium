bca2a99c11e3605166e510f1b2ed3191c856a17a
DefinitelyNotSidu <b.magdz@gmail.com>
2023-10-04 23:31:09 +0200

    JoinEvent i proby robienia miecza (nie dziala XD)

--- src/me/DefinitelyNotSidu/Toxium/Items/ToxiumSword.java
+++ src/me/DefinitelyNotSidu/Toxium/Items/ToxiumSword.java
@@ -1,4 +1,39 @@
 package me.DefinitelyNotSidu.Toxium.Items;
 
-public class ToxiumSword {
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class ToxiumSword extends JavaPlugin {
+
+    public static ShapedRecipe sword() {
+        ItemStack sword = new ItemStack(Material.NETHERITE_SWORD);
+        ItemMeta meta = sword.getItemMeta();
+
+        meta.setCustomModelData(6);
+        meta.setDisplayName(ChatColor.RESET + "" + ChatColor.GREEN + "Toxium Sword");
+        //TODO: damage
+        sword.setItemMeta(meta);
+
+        NamespacedKey key = new NamespacedKey(this, "toxium_sword");
+        //to sie pierdoli, jak wrócę to zobaczę jak to obejść XD
+
+        ShapedRecipe recipe = new ShapedRecipe(key, sword);
+
+        recipe.shape(" I ", " I ", " S ");
+        recipe.setIngredient('I', Material.IRON_INGOT);
+        recipe.setIngredient('S', Material.STICK);
+
+
+
+        return recipe;
+    }
+
+
 }
--- src/me/DefinitelyNotSidu/Toxium/Main.java
+++ src/me/DefinitelyNotSidu/Toxium/Main.java
@@ -1,11 +1,16 @@
 package me.DefinitelyNotSidu.Toxium;
 
+import me.DefinitelyNotSidu.Toxium.EventHandler.JoinEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import me.DefinitelyNotSidu.Toxium.Items.ToxiumSword;
 
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
-
+        Bukkit.getServer().getPluginManager().registerEvents(new JoinEvent(), this);
+        Bukkit.addRecipe(ToxiumSword.sword());
+        //recka nie działa, jeszcze to poprawię XD bo się pierdolę żeby to zrobić w oddzielnych plikach xD
     }
     @Override
     public void onDisable() {
