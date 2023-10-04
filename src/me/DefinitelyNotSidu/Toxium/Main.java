105ff2fffa9b1a97660c8f0fbd06d21a9baacbe3
DefinitelyNotSidu <b.magdz@gmail.com>
2023-10-04 22:40:07 +0200

    initial commit

---
+++ src/me/DefinitelyNotSidu/Toxium/Main.java
@@ -0,0 +1,14 @@
+package me.DefinitelyNotSidu.Toxium;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+
+
+
+}
